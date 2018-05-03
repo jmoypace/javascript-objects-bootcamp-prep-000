4f4d6ad57f669a5064fe240309c824d0fbdfac28
jmoypace <jmoypace@gmail.com>
2018-05-03 03:31:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,6 @@ var playlist={artistname:"song"};
 function updatePlaylist(playlist,artistname,songTitle){
   playlist[artistname]=songTitle
 }
+function removeFromtPlaylist(playlist,artistname){
+  delete playlist.artistname;
+}
